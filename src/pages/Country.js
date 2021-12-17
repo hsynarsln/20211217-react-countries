2645bc47f280c069d018b4ef647c3093fb76217d
hsynarslan <hsyn06arslan@gmail.com>
2021-12-17 19:00:48 +0300

    expanded panel

--- src/pages/Country.js
+++ src/pages/Country.js
@@ -1,15 +1,49 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
 import Avatar from '@mui/material/Avatar';
 import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
+import Collapse from '@mui/material/Collapse';
 import { red } from '@mui/material/colors';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import '../App.css';
 
-const Country = ({ country }) => {
-  // console.log({ country });
+const ExpandMore = styled(props => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest
+  })
+}));
+
+const Country = props => {
+  // console.log(props);
+  const {
+    flag,
+    capital,
+    name: { common: countryName },
+    flags,
+    population,
+    region,
+    maps: { openStreetMaps: countryMap },
+    borders
+  } = props.country;
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
     <div>
@@ -17,22 +51,51 @@ const Country = ({ country }) => {
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
-              {country.flag}
+              {flag}
             </Avatar>
           }
-          title={country.name.common}
-          subheader={country.capital}
+          title={countryName}
+          subheader={capital}
         />
-        <CardMedia component='img' height='194' image={country.flags[0]} alt={country.name.common} />
+        <CardMedia component='img' height='194' image={flags[0]} alt={countryName} />
         <CardContent>
-          <Typography variant='body2' color='text.secondary'>
-            Population: {country.population}
-            <br />
-            Region: {country.region}
-            <br />
-            {/* Languages: {country.languages} */}
+          <Typography align='justify' variant='body2' color='text.secondary'>
+            Population: {population}
           </Typography>
+          <Typography align='justify' variant='body2' color='text.secondary'>
+            Region: {region}
+          </Typography>
+          {borders ? (
+            <Typography align='justify' variant='body2' color='text.secondary'>
+              Neigbours: {borders.join(',')}
+            </Typography>
+          ) : (
+            <Typography align='justify' variant='body2' color='text.secondary'>
+              No Neighbours
+            </Typography>
+          )}
+          {/* <Typography align='justify' variant='body2' color='text.secondary'>
+            Languages: {languages}
+          </Typography> */}
         </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label='add to favorites'>
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label='share'>
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label='show more'>
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout='auto' unmountOnExit>
+          <CardContent>
+            <Typography paragraph>
+              <iframe width='260' height='250' src={countryMap} frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen title='Embedded youtube' />
+            </Typography>
+          </CardContent>
+        </Collapse>
       </Card>
     </div>
   );
